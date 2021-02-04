2b83529c51af996c633a552171bd3af77496b087
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2021-02-04 10:14:26 -0500

    migration fix?

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
 
+const pg = require("pg");
+pg.defaults.ssl = true;
+
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL

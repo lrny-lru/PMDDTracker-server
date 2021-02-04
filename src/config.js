9b55c00777f960326eb7bf65788759ceccc6d6da
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2021-02-03 22:29:43 -0500

    migrate??

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'd3460822-1e38-11eb-adc1-0242ac120002',
-    DB_URL: process.env.DB_URL || 'postgresql://Larue@localhost/PMDD',
+    DATABASE_URL: process.env.DB_URL || 'postgres://ulkwnslikwdqtd:58cdf431a83aeb28c20f335ed4e7941e8177785eba9ee31c8c3033c9b7d0ec60@ec2-3-222-11-129.compute-1.amazonaws.com:5432/d3id04k7m4911u',
   }
